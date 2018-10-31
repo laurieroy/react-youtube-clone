97050db597b9ac36005805cc5f30b9c7cd9b7ba1
Laurie Roy <laurieroy.dev@gmail.com>
2018-10-30 23:33:35 -0700

    YTSearch, generate list of videos, thumbnail on left, title as headig on rt

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyBqAOQApsVUJdnKPKnmdYqIPluM3vGdSlo';
 
-// Create a new component. This component should prodcue some html
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
-};
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-// Take this component's generated HTML and put it in the DOM
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+            this.setState({ videos });
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
+}
+
 ReactDOM.render(<App />, document.querySelector('.container'));
