9612d1f1a30ff9931849760ffcffbfc563dfcb64
Laurie Roy <laurieroy.dev@gmail.com>
2018-10-31 00:45:29 -0700

    Add styling

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -5,9 +5,9 @@ const VideoDetail = ({ video }) => {
     if (!video) {
         return <div>Loading...</div>;
     }
-    
+
     const videoId = video.id.videoId;
-    const url = `htpps://www.youtube.com/embed/${videoId}`;
+    const url = `https://www.youtube.com/embed/${videoId}`;
 
     return (
         <div className="video-detail col-md-8">
